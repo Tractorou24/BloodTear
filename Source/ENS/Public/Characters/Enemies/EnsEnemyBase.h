e96fb465c52da6a47349434c5e0adb63c6a2a236
Fabian INGREMEAU <fabian.ingremeau@outlook.com>
2025-06-02 17:02:41 +0200

    add ragdoll & death effects for enemies

--- Source/ENS/Public/Characters/Enemies/EnsEnemyBase.h
+++ Source/ENS/Public/Characters/Enemies/EnsEnemyBase.h
@@ -2,12 +2,11 @@
 
 #pragma once
 
-#include "Characters/EnsCharacterBase.h"
 #include "CoreMinimal.h"
 #include "GenericTeamAgentInterface.h"
+#include "Characters/EnsCharacterBase.h"
 
 #include "EnsEnemyBase.generated.h"
-
 
 DECLARE_LOG_CATEGORY_EXTERN(LogEnemy, Log, All);
 
@@ -25,69 +24,69 @@ DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnEnemyDestroyed);
 UCLASS()
 class ENS_API AEnsEnemyBase : public AEnsCharacterBase, public IGenericTeamAgentInterface
 {
-    GENERATED_BODY()
+	GENERATED_BODY()
 
 public:
-    explicit AEnsEnemyBase();
+	explicit AEnsEnemyBase();
 
-    /**
-     * \brief Sets the team the current enemy is in.
-     * \param NewTeamID The id of the new team.
-     */
-    UFUNCTION(BlueprintCallable)
-    virtual void SetGenericTeamId(const FGenericTeamId& NewTeamID) override;
+	/**
+	 * \brief Sets the team the current enemy is in.
+	 * \param NewTeamID The id of the new team.
+	 */
+	UFUNCTION(BlueprintCallable)
+	virtual void SetGenericTeamId(const FGenericTeamId& NewTeamID) override;
 
-    /// \brief Gets the id of the team the enemy is currently in.
-    UFUNCTION(BlueprintCallable)
-    virtual FGenericTeamId GetGenericTeamId() const override;
+	/// \brief Gets the id of the team the enemy is currently in.
+	UFUNCTION(BlueprintCallable)
+	virtual FGenericTeamId GetGenericTeamId() const override;
 
-    /// \brief Called when the enemy is destroyed.
-    UPROPERTY(BlueprintAssignable)
-    FOnEnemyDestroyed OnEnemyDestroyed;
+	/// \brief Called when the enemy is destroyed.
+	UPROPERTY(BlueprintAssignable)
+	FOnEnemyDestroyed OnEnemyDestroyed;
 
 protected:
-    virtual void BeginPlay() override;
+	virtual void BeginPlay() override;
 
-    /// \brief Kills the enemy.
-    virtual void OnDeath(AEnsCharacterBase* SourceActor) override;
+	/// \brief Kills the enemy.
+	virtual void OnDeath(AEnsCharacterBase* SourceActor) override;
 
 	/// \copydoc AEnsCharacterBase::HealthChanged
 	virtual void HealthChanged(const FOnAttributeChangeData& Data) override;
 
-    /**
-     * @brief OnDeath blueprint event called by OnDeath native class
-     * Unreal does not accepts Blueprint Implementable/Native events to be virtual
-     */
-    UFUNCTION(BlueprintImplementableEvent)
-    void OnDeath_Blueprint();
+	/**
+	 * @brief OnDeath blueprint event called by OnDeath native class
+	 * Unreal does not accepts Blueprint Implementable/Native events to be virtual
+	 */
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnDeath_Blueprint();
 
 private:
-    /**
-     * \brief Called when the enemy is attacked.
-     * \param Source The actor that attacked.
-     */
-    UFUNCTION()
-    void Attacked(AActor* Source);
+	/**
+	 * \brief Called when the enemy is attacked.
+	 * \param Source The actor that attacked.
+	 */
+	UFUNCTION()
+	void Attacked(AActor* Source);
 
 	/**
-    * \brief Initializes the UI for the enemy.
+     * \brief Initializes the UI for the enemy.
 	 */
 	UFUNCTION()
 	void InitUI();
 
-    FGenericTeamId TeamId;
+	FGenericTeamId TeamId;
 
-    /// \brief The interaction component.
-    UPROPERTY(EditAnywhere, Category = "Interactions", meta = (AllowPrivateAccess = "true"))
-    class UEnsMouseInteractableComponent* MouseInteractableComponent = nullptr;
+	/// \brief The interaction component.
+	UPROPERTY(EditAnywhere, Category = "Interactions", meta = (AllowPrivateAccess = "true"))
+	class UEnsMouseInteractableComponent* MouseInteractableComponent = nullptr;
 
-    /// \brief The interaction zone of the enemy (i.e. where it can interact after moving)
-    UPROPERTY(EditAnywhere, Category = "Interactions", meta = (AllowPrivateAccess = "true"))
-    class UBoxComponent* InteractZone = nullptr;
+	/// \brief The interaction zone of the enemy (i.e. where it can interact after moving)
+	UPROPERTY(EditAnywhere, Category = "Interactions", meta = (AllowPrivateAccess = "true"))
+	class UBoxComponent* InteractZone = nullptr;
 
-    /// \brief The zone the player can click into to interact with the enemy.
-    UPROPERTY(EditAnywhere, Category = "Interactions", meta = (AllowPrivateAccess = "true"))
-    class UBoxComponent* InteractionClickZone = nullptr;
+	/// \brief The zone the player can click into to interact with the enemy.
+	UPROPERTY(EditAnywhere, Category = "Interactions", meta = (AllowPrivateAccess = "true"))
+	class UBoxComponent* InteractionClickZone = nullptr;
 
 	/// \brief The class used to display the enemy's information in the UI.
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "UI", meta = (AllowPrivateAccess = "true"))
@@ -97,9 +96,17 @@ private:
 	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "UI", meta = (AllowPrivateAccess = "true"))
 	class UWidgetComponent* UIEnemyInfoComponent = nullptr;
 
-    /// \brief The number of XP points gained by killing this enemy.
-    UPROPERTY(EditDefaultsOnly, Category = "Levels", meta = (AllowPrivateAccess = "true"))
-    int64 GivenExperience = 10;
+	/// \brief The effect to spawn when the enemy dies.
+	UPROPERTY(EditAnywhere, Category = "Death", meta = (AllowPrivateAccess = "true"))
+	class UNiagaraSystem* DeathEffect = nullptr;
+
+	/// \brief The duration of the death effect.
+	UPROPERTY(EditDefaultsOnly, Category = "Death", meta = (AllowPrivateAccess = "true", ClampMin = "0.0", UIMin = "0.0"))
+	float DeathEffectDuration = 0.5f;
+
+	/// \brief The number of XP points gained by killing this enemy.
+	UPROPERTY(EditDefaultsOnly, Category = "Levels", meta = (AllowPrivateAccess = "true"))
+	int64 GivenExperience = 10;
 
 	UPROPERTY()
 	UEnemyInfo* UIEnemyInfo = nullptr;
