922c5606fec4dcb24cdb4072ce0c171ca332be62
Fabian INGREMEAU <fabian.ingremeau@outlook.com>
2025-06-04 15:54:57 +0200

    weapon mesh per level + left hand

--- Source/ENS/Private/Equipment/BaseWeapon.cpp
+++ Source/ENS/Private/Equipment/BaseWeapon.cpp
@@ -1,8 +1,32 @@
 ﻿// Copyright (c) 2024-2025, BloodTear contributors. All rights reserved.
 
 #include "Equipment/BaseWeapon.h"
+#include "Characters/Player/EnsPlayerCharacter.h"
 
 ABaseWeapon::ABaseWeapon()
 {
-    Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+    LeftHandMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("LeftHandWeaponMesh"));
+    LeftHandMeshComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+    RightHandMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RightHandWeaponMesh"));
+    RightHandMeshComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+void ABaseWeapon::InitAndAttach(const AEnsPlayerCharacter* Character)
+{
+    ensure(Character != nullptr && "Character must not be null");
+
+    const auto Level = Character->GetCurrentLevel() - 1;
+    auto LeftHandMesh = MeshLeftHandPerLevel.IsValidIndex(Level) ? MeshLeftHandPerLevel[Level] : nullptr;
+    auto RightHandMesh = MeshRightHandPerLevel.IsValidIndex(Level) ? MeshRightHandPerLevel[Level] : nullptr;
+
+    ensure(RightHandMesh != nullptr && "Left hand weapon mesh must not be null"); // Ensure the left hand mesh is valid
+    RightHandMeshComponent->SetStaticMesh(RightHandMesh);
+    RightHandMeshComponent->AttachToComponent(Character->GetMesh(), FAttachmentTransformRules::SnapToTargetNotIncludingScale, RightHandSocketName);
+
+    if (LeftHandMesh)
+    {
+        LeftHandMeshComponent->SetStaticMesh(LeftHandMesh);
+        LeftHandMeshComponent->AttachToComponent(Character->GetMesh(), FAttachmentTransformRules::SnapToTargetNotIncludingScale, LeftHandSocketName);
+    }
 }
