e35099f4af2764d487e62320812cc0ed71591f3d
Kévin Granger <71252880+Kraizix@users.noreply.github.com>
2025-06-04 11:11:25 +0200

    controls (#134)
    Co-authored-by: Fabian INGREMEAU <fabian.ingremeau@outlook.com>

--- Source/ENS/Private/Characters/Player/EnsPlayerController.cpp
+++ Source/ENS/Private/Characters/Player/EnsPlayerController.cpp
@@ -9,89 +9,90 @@
 
 AEnsPlayerController::AEnsPlayerController()
 {
-	// Show mouse cursor, so we can click to move
-	bShowMouseCursor = true;
-	DefaultMouseCursor = EMouseCursor::Default;
+    // Show mouse cursor, so we can click to move
+    bShowMouseCursor = true;
+    DefaultMouseCursor = EMouseCursor::Default;
 }
 
 void AEnsPlayerController::BeginPlay()
 {
-	Super::BeginPlay();
+    Super::BeginPlay();
 }
 
 void AEnsPlayerController::OnPossess(APawn* InPawn)
 {
-	Super::OnPossess(InPawn);
+    Super::OnPossess(InPawn);
 
-	auto* PlayerCharacter = Cast<AEnsPlayerCharacter>(GetCharacter());
-	if (!PlayerCharacter)
-	{
-		UE_LOG(LogPlayerController, Error, TEXT("Failed to get the player character for setting actor abilities."));
-		return;
-	}
+    auto* PlayerCharacter = Cast<AEnsPlayerCharacter>(GetCharacter());
+    if (!PlayerCharacter)
+    {
+        UE_LOG(LogPlayerController, Error, TEXT("Failed to get the player character for setting actor abilities."));
+        return;
+    }
 
-	PlayerCharacter->GetAbilitySystemComponent()->InitAbilityActorInfo(PlayerCharacter, InPawn);
+    PlayerCharacter->GetAbilitySystemComponent()->InitAbilityActorInfo(PlayerCharacter, InPawn);
 
-	PlayerCharacter->OnDeathDelegate.AddUniqueDynamic(this, &AEnsPlayerController::OnDeath);
+    PlayerCharacter->OnDeathDelegate.AddUniqueDynamic(this, &AEnsPlayerController::OnDeath);
 }
 
 void AEnsPlayerController::OnDeath_Implementation()
 {
-	if (Lives > 1)
-	{
-		Lives--;
-		LostLife();
-	} else
-		Restart();
+    if (Lives > 1)
+    {
+        Lives--;
+        LostLife();
+    }
+    else
+        Restart();
 }
 
 void AEnsPlayerController::Tick(const float DeltaSeconds)
 {
-	Super::Tick(DeltaSeconds);
+    Super::Tick(DeltaSeconds);
 
-	// Update mouse position (if in game)
-	if (GetWorld() && (GetWorld()->WorldType == EWorldType::Game || GetWorld()->WorldType == EWorldType::GamePreview || GetWorld()->WorldType == EWorldType::PIE))
-	{
-		// Get the current viewport size
-		FVector2D ViewportSize;
-		if (!GEngine && !GEngine->GameViewport)
-		{
-			UE_LOG(LogPlayerController, Error, TEXT("Failed to get the viewport size. No engine or viewport."));
-			return;
-		}
-		GEngine->GameViewport->GetViewportSize(ViewportSize);
+    // Update mouse position (if in game)
+    if (GetWorld() && (GetWorld()->WorldType == EWorldType::Game || GetWorld()->WorldType == EWorldType::GamePreview || GetWorld()->WorldType == EWorldType::PIE))
+    {
+        // Get the current viewport size
+        FVector2D ViewportSize;
+        if (!GEngine && !GEngine->GameViewport)
+        {
+            UE_LOG(LogPlayerController, Error, TEXT("Failed to get the viewport size. No engine or viewport."));
+            return;
+        }
+        GEngine->GameViewport->GetViewportSize(ViewportSize);
 
-		GetMousePosition(RelativeMousePosition.X, RelativeMousePosition.Y);
-		RelativeMousePosition /= ViewportSize;
-	}
+        GetMousePosition(RelativeMousePosition.X, RelativeMousePosition.Y);
+        RelativeMousePosition /= ViewportSize;
+    }
 }
 
 void AEnsPlayerController::SetupInputComponent()
 {
-	Super::SetupInputComponent();
+    Super::SetupInputComponent();
 
-	// Add Input Mapping Context
-	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
-		Subsystem->AddMappingContext(DefaultMappingContext, 0);
+    // Add Input Mapping Context
+    if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+        Subsystem->AddMappingContext(DefaultMappingContext, 0);
 
-	UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent);
-	if (!EnhancedInputComponent)
-	{
-		UE_LOG(LogPlayerController, Error, TEXT("'%s' Failed to find an Enhanced Input Component!"), *GetNameSafe(this));
-		return;
-	}
+    UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent);
+    if (!EnhancedInputComponent)
+    {
+        UE_LOG(LogPlayerController, Error, TEXT("'%s' Failed to find an Enhanced Input Component!"), *GetNameSafe(this));
+        return;
+    }
 
-	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AEnsPlayerController::Move);
-	EnhancedInputComponent->BindAction(AttackAction, ETriggerEvent::Started, this, &AEnsPlayerController::Attack);
+    EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AEnsPlayerController::Move);
+    EnhancedInputComponent->BindAction(AttackAction, ETriggerEvent::Started, this, &AEnsPlayerController::Attack);
 }
 
 void AEnsPlayerController::Move(const FInputActionValue& Value)
 {
-	auto* PlayerCharacter = Cast<AEnsPlayerCharacter>(GetCharacter());
-	PlayerCharacter->Move(Value.Get<FVector2D>());
+    auto* PlayerCharacter = Cast<AEnsPlayerCharacter>(GetCharacter());
+    PlayerCharacter->Move(Value.Get<FVector2D>());
 }
 
 void AEnsPlayerController::Attack()
 {
-	Cast<AEnsPlayerCharacter>(GetCharacter())->BaseAttack();
+    Cast<AEnsPlayerCharacter>(GetCharacter())->BaseAttack();
 }
--- Source/ENS/Private/UI/EnemyInfo.cpp
+++ Source/ENS/Private/UI/EnemyInfo.cpp
@@ -1,6 +1,5 @@
 // Copyright (c) 2024-2025, BloodTear contributors. All rights reserved.
 
-
 #include "UI/EnemyInfo.h"
 
 void UEnemyInfo::NativeConstruct()
