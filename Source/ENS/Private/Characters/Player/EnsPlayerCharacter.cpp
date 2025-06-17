a6c49435a0331a23a39125d925350ec81555e610
Dorian Fonseca <71253089+Dorian2002@users.noreply.github.com>
2025-06-17 17:31:36 +0200

    Fix : checkpoint new update with arena entry

--- Source/ENS/Private/Characters/Player/EnsPlayerCharacter.cpp
+++ Source/ENS/Private/Characters/Player/EnsPlayerCharacter.cpp
@@ -97,7 +97,7 @@ void AEnsPlayerCharacter::OnDeath(AEnsCharacterBase* SourceActor)
     FTimerDelegate TimerCallback;
     TimerCallback.BindLambda([=, this] {
         // Find and move to the player start
-        const AActor* PlayerStart = GetWorld()->GetAuthGameMode()->FindPlayerStart(GetController());
+        const AActor* PlayerStart = GetWorld()->GetAuthGameMode()->FindPlayerStart(GetController(),FString("Checkpoint"));
         SetActorLocation(PlayerStart->GetActorLocation());
         SetActorRotation(PlayerStart->GetActorRotation());
 
